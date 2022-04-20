03ac7c6b40f00424208e1e92d9329bdde5ba7623
midavwork <midav.work+github@gmail.com>
2022-04-20 20:41:40 +0300

    Inainte de a scoate React - downloadat local mai intai; adaugat modulul de email editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, ViewChild } from '@angular/core';
+import { EmailEditorComponent } from 'angular-email-editor';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  title = 'angular-email-editor';
   name = 'Angular ' + VERSION.major;
+
+  @ViewChild(EmailEditorComponent)
+  private emailEditor: EmailEditorComponent;
+
+  editorLoaded($event) {
+    // load the design json here
+    // this.emailEditor.editor.loadDesign({});
+    console.log('S-a incarcat editorul de email!!!');
+  }
+
+  exportHtml() {
+    this.emailEditor.editor.exportHtml((data) =>
+      console.log('exportHtml', data)
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { EmailEditorModule } from 'angular-email-editor';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, EmailEditorModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
